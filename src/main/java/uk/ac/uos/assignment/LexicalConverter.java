c194fd2ca0e6d0557727e589be520ff2edcef68c
Brennan Minns <s195215@uos.ac.uk>
2018-11-12 20:24:34 +0000

    added JSONSymbol

---
+++ src/main/java/uk/ac/uos/assignment/LexicalConverter.java
@@ -0,0 +1,13 @@
+package uk.ac.uos.assignment;
+
+import java.io.StringReader;
+import java.io.PushbackReader;
+import java.io.IOException;
+
+
+//This class will convert the syntax of string formatted JSON into the
+//correct symbol type
+public class LexicalConverter {
+	
+	//Pushback reader allows me to read ahead of
+}
